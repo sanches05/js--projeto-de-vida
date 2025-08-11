d97bec0813e983d7d2853d4a0a9a9789b62f80df
sanches05 <161397879+sanches05@users.noreply.github.com>
2025-08-11 22:14:38 +0000

    867

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-07-05T00:00:00");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-01T00:00:00");
 const tempoObjetivo4 = new Date("2025-12-15T00:00:00");
